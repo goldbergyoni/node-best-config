b7f847ba4ca6b60ae50de8cf2acd92ee23449853
Bruno Scheufler <bszerofive@gmail.com>
2018-06-06 22:20:12 +0200

    Replaced nconf package config with convict config

--- packages/nconf/client.js
+++ packages/nconf/client.js
@@ -7,4 +7,8 @@ nconf
 	.file({ file: 'config.json' }) // Load default configuration file
 	.file({ file: 'overrides.json' }); // Load overrides
 
+// Overriden by environment variable
 console.log(nconf.get('database'));
+
+// Overriden by overrides file
+console.log(nconf.get('db'));
