25efcfdb5caddad7bc4cc6fbad08e869624dffe8
Bruno Scheufler <bszerofive@gmail.com>
2018-06-06 22:17:34 +0200

    Replaced configuration of package config with convict example configuration

--- packages/config/client.js
+++ packages/config/client.js
@@ -2,5 +2,8 @@ const path = require('path');
 const config = require('config');
 
 // Read nested configuration property
-const dbConfig = config.get('Customer.dbConfig');
+const dbConfig = config.get('db.host');
 console.log(dbConfig);
+
+// Read ip which should be overriden by the environment variable in the start script
+console.log(config.get('ip'));
